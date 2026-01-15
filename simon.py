2497b35239a92fc508e552811f68be5e341dfb2d
LenyBl <blee.leny@gmail.com>
2026-01-15 21:50:39 +0100

    refactor: Improve checkResult and checkError functions for clarity and efficiency

--- simon.py
+++ simon.py
@@ -11,35 +11,68 @@ def clearTerminal():
 	os.system('clear' if os.name == "posix" else "cls")
 
 def checkResult(userInput, listNumbers):
-	cmpListNumbers = ""
-	for k in listNumbers:
-		cmpListNumbers += str(k)
+	listUserInput = []
+	for i in userInput:
+		listUserInput.append(int(i))
 
-	for i in listNumbers:
-		for j in userInput:
-			if i == j:
-				continue
-			else:
-				return False
-	return True
+	for j in range(0, len(listNumbers)):
+		if listNumbers[j] == listUserInput[j]:
+			result = True
+		else:
+			result = False
+	return result
+
+def checkError(input):
+	listUserInput = []
+	for i in input:
+		listUserInput.append(i)
+
+	for j in range(0, len(listUserInput)):
+		if listUserInput[j] >= '0' and listUserInput[j] <= '9':
+			result = True
+		else:
+			return False
+	return result
+
+def difficult(difficulty):
+	match difficulty:
+		case "easy":
+			return [5, 1]
+		case "medium":
+			return [3, 2]
+		case "hard":
+			return [2, 4]
+		case "impossible":
+			return [1, 10]
 
 list_numbers = []
 status = True
 correctAnswer = 0
 
-
 for i in range(0,4):
 	list_numbers.append(randrange(10))
 
+userInputDifficulty = input("Quel difficulté voulez vous choisir ? (easy, medium, hard, impossible) : ")
+
 while status:
+	listValueDifficulty = difficult(userInputDifficulty)
+	print(listValueDifficulty)
 	showNumbers(list_numbers)
-	time.sleep(2)
+	time.sleep(listValueDifficulty[0])
 	clearTerminal()
 	userInput = input("Quelle est la liste de nombre ? : ")
-	status = checkResult(userInput, list_numbers)
-	print(status)
-	if status:
-		correctAnswer += 1
-		list_numbers.append(randrange(10))
+	error = checkError(userInput)
+	if error == True:
+		if len(userInput) == len(list_numbers):
+			status = checkResult(userInput, list_numbers)
+			if status:
+				correctAnswer += 1
+				for i in range(0, listValueDifficulty[1]):
+					list_numbers.append(randrange(10))
+			else: 
+				print(f"Vous avez perdu ! Votre score est de {correctAnswer} !")
+		else:
+			print("La taille de saisie n'est pas correct !")
 	else: 
-		print(f"Vous avez perdu ! Votre score est de {correctAnswer}")
+		print("La saisie contient des caractères autres que des chiffres !")
+		
